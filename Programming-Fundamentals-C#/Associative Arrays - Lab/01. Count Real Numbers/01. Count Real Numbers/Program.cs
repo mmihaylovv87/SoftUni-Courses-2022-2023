9c4daff63555e4f3f59ceb3bd8f6e3ce004ea021
mmihaylovv87 <mmihaylovv87@abv.bg>
2023-01-12 13:01:28 +0200

    Added Programming Fundamentals C# - Associative Arrays - Lab code.

---
+++ Programming-Fundamentals-C#/Associative Arrays - Lab/01. Count Real Numbers/01. Count Real Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, int> numbers = new SortedDictionary<int, int>();
+
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            foreach (var num in nums)
+            {
+                if (!numbers.ContainsKey(num))
+                {
+                    numbers.Add(num, 0);
+                }
+                numbers[num]++;
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals-C#/Associative Arrays - Lab/02. Odd Occurrences/02. Odd Occurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .Select(word => word.ToLower())
+                .ToArray();
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (!wordCounts.ContainsKey(word))
+                {
+                    wordCounts.Add(word, 0);
+                }
+
+                wordCounts[word]++;
+            }
+
+            string[] oddCountWords = wordCounts
+                .Where(w => w.Value % 2 != 0)
+                .Select(w => w.Key)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", oddCountWords));
+        }
+    }
+}
