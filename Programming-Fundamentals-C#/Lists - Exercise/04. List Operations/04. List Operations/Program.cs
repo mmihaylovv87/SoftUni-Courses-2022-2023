5df6d6948d2a97d9116b78878f0f6fa0ccb14674
mmihaylovv87 <mmihaylovv87@abv.bg>
2023-01-12 13:58:07 +0200

    Added Programming Fundamentals C# - Lists-Exercise code.

---
+++ Programming-Fundamentals-C#/Lists - Exercise/04. List Operations/04. List Operations/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._List_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] tokens = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (tokens[0] == "Add")
+                {
+                    int number = int.Parse(tokens[1]);
+                    numbers.Add(number);
+                }
+                else if (tokens[0] == "Insert")
+                {
+                    int number = int.Parse(tokens[1]);
+                    int index = int.Parse(tokens[2]);
+                    if (index >= numbers.Count || index < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+                    else
+                    {
+                        numbers.Insert(index, number);
+                    }
+                }
+                else if (tokens[0] == "Remove")
+                {
+                    int index = int.Parse(tokens[1]);
+                    if (index >= numbers.Count || index < 0)
+                    {
+
+                        Console.WriteLine("Invalid index");
+                    }
+                    else
+                    {
+                        numbers.RemoveAt(index);
+                    }
+                }
+                else if (tokens[0] == "Shift")
+                {
+                    int count = int.Parse(tokens[2]);
+                    if (tokens[1] == "left")
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            int firstNumber = numbers[0];
+                            numbers.RemoveAt(0);
+                            numbers.Add(firstNumber);
+                        }
+                    }
+                    else if (tokens[1] == "right")
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            int lastNumber = numbers[numbers.Count - 1];
+                            numbers.RemoveAt(numbers.Count - 1);
+                            numbers.Insert(0, lastNumber);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ Programming-Fundamentals-C#/Lists - Exercise/10. SoftUni Course Planning/10. SoftUni Course Planning/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+
+namespace _10._SoftUni_Course_Planning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lessonTitles = Console.ReadLine().Split(", ").ToList();
+            Dictionary<int, string> lessonsAfterAllCommands = new Dictionary<int, string>();
+            
+            for (int i = 0; i < lessonTitles.Count; i++)
+            {
+                lessonsAfterAllCommands.Add(i + 1, lessonTitles[i]);
+            }
+            
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split(":");
+                string command = commands[0];
+                string lesson = commands[1];
+                if (command == "course")
+                    break;
+
+                if (command == "Swap")
+                {
+                    string secondLesson = commands[2];
+                    
+                }
+                
+            }
+        }
+    }
+}
