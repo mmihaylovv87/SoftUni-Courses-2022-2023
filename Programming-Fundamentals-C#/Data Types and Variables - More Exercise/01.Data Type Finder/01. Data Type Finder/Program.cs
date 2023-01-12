3babbb6081719d3b3c63998d240fd6775cec5dad
mmihaylovv87 <mmihaylovv87@abv.bg>
2023-01-12 13:49:43 +0200

    Added Programming Fundamentals C# - Data Types and Variables-More Exercise code.

---
+++ Programming-Fundamentals-C#/Data Types and Variables - More Exercise/01.Data Type Finder/01. Data Type Finder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Data_Type_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (input == "True" || input == "False")
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else if (int.Parse(input) * 0 == 0)
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (double.Parse(input) * 0 == 0)
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (input.Length < 2)
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programming-Fundamentals-C#/Data Types and Variables - More Exercise/02.FromLeftToRight/02.FromLeftToTheRight/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._From_Left_to_The_Right
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                int sum = 0;
+                List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+                int firstNumber = numbers[0];
+                int secondNumber = numbers[1];
+                if (firstNumber >= secondNumber)
+                {
+                    while (firstNumber > 0)
+                    {
+                        sum += firstNumber % 10;
+                        firstNumber /= 10;
+                    }
+                }
+                else
+                {
+                    while (secondNumber > 0)
+                    {
+                        sum += secondNumber % 10;
+                        secondNumber /= 10;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
