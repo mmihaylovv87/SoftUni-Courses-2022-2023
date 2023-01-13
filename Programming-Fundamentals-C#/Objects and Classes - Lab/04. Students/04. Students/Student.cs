8b513433c0b84c646fb3760815cfcc147167476e
mmihaylovv87 <mmihaylovv87@abv.bg>
2023-01-13 22:56:31 +0200

    Added Programming Fundamentals C# - Object and Classes-Lab code.

---
+++ Programming-Fundamentals-C#/Objects and Classes - Lab/04. Students/04. Students/Student.cs
@@ -0,0 +1,6 @@
+﻿namespace _04._Students
+{
+    internal class Student
+    {
+    }
+}
---
+++ Programming-Fundamentals-C#/Objects and Classes - Lab/05. Students 2.0/05. Students 2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05._Students_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
