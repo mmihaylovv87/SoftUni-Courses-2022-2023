f008a0c8432f3b34bcb345ec4f532044680ca32c
mmihaylovv87 <mmihaylovv87@abv.bg>
2023-01-13 22:57:18 +0200

    Added Programming Fundamentals C# - Object and Classes-Exercise code.

---
+++ Programming-Fundamentals-C#/Objects and Classes - Exercise/05. Teamwork Projects/demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> teams = new Dictionary<string, string>();
+            teams.Add("Ivan", "Petrov");
+            teams.Add("Petar", "Hristov");
+            Console.WriteLine(teams.Keys.Contains("Ivan"));
+        }
+    }
+}
---
+++ Programming-Fundamentals-C#/Objects and Classes - Exercise/4. Students/4. Students/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfStudent = int.Parse(Console.ReadLine());
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < countOfStudent; i++)
+            {
+                string[] currStudentInfo = Console.ReadLine().Split(" ");
+                Student student = new Student(currStudentInfo[0], currStudentInfo[1], double.Parse(currStudentInfo[2]));
+                students.Add(student);
+            }
+            students = students.OrderByDescending(currStudent => currStudent.Grade).ToList();
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+        public override string ToString() => $"{FirstName} {LastName}: {Grade:f2}";
+    }
+}
