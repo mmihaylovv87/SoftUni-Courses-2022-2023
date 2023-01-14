cb98c60652b2807abc31f8a3d90bfee4b3324db1
mmihaylovv87 <mmihaylovv87@abv.bg>
2023-01-14 07:33:47 +0200

    Added Programming Fundamentals C# - Text Processing-Lab code.

---
+++ Programming-Fundamentals-C#/Text Processing - Lab/04. Text Filter/04. Text Filter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var bannedWord in bannedWords)
+            {
+                string replacedWithSymbol = new string('*', bannedWord.Length);
+                text = text.Replace(bannedWord, replacedWithSymbol);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ Programming-Fundamentals-C#/Text Processing - Lab/05. Digits, Letters/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input.Where(ch => char.IsDigit(ch)).ToArray());
+            Console.WriteLine(input.Where(ch => char.IsLetter(ch)).ToArray());
+            Console.WriteLine(input.Where(ch => !char.IsDigit(ch) && !char.IsLetter(ch)).ToArray());
+        }
+    }
+}
