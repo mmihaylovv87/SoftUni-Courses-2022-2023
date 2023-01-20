eb4e5484c05f68d8fa8b2fd7b23ec3cae3018096
mmihaylovv87 <mmihaylovv87@abv.bg>
2023-01-20 23:41:44 +0200

    Added Advanced C# - Defining Classes-Exercise code.

---
+++ Advanced-C#/C#Advanced/Defining Classes - Exercise/03. Oldest Family Member/Family.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            people = new List<Person>();
+        }
+
+        public List<Person> People
+        {
+            get { return this.people; }
+            set { this.people = value; }
+        }
+
+        public void AddMember(Person person)
+        {
+            this.people.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.people
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+        }
+    }
+
+}
---
+++ Advanced-C#/C#Advanced/Defining Classes - Exercise/06. Speed Racing/SpeedRacing.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Speed_Racing
+{
+    public class SpeedRacing
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Car> carsByNames = new Dictionary<string, Car>();
+            
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] carsInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Car car = new Car
+                {
+                    Model = carsInfo[0],
+                    FuelAmount = double.Parse(carsInfo[1]),
+                    FuelConsumptionPerKilometer = double.Parse(carsInfo[2])
+                };
+                carsByNames.Add(car.Model, car);
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                    break;
+
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string carModel = tokens[1];
+                double amountOfKm = double.Parse(tokens[2]); 
+
+                Car car = carsByNames[carModel];
+                car.Drive(amountOfKm);
+            }
+
+            foreach (var car in carsByNames.Values)
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+        }
+    }
+}
