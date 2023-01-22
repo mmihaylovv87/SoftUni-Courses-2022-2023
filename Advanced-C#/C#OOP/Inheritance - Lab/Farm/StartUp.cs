c4eb00bf39d09ee65f57d376f13221e0c64c57f4
mmihaylovv87 <mmihaylovv87@abv.bg>
2023-01-22 15:12:30 +0200

    Added OOP-C# - Inheritance-Lab code.

---
+++ Advanced-C#/C#OOP/Inheritance - Lab/Farm/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
+            Cat cat = new Cat();
+            cat.Meow();
+        }
+    }
+}
