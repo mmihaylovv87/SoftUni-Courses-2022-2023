11b078ac381f814774948a1db113ded8bf39bf25
mmihaylovv87 <111530970+mmihaylovv87@users.noreply.github.com>
2023-01-29 23:48:54 +0200

    Added OOP-C# - Reflection&Attributes-Lab code.

---
+++ Advanced-C#/C#OOP/Reflection and Attributes - Lab/03. Mission Private Impossible/Spy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string RevealPrivateMethods(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"All Private Methods of Class: {className}");
+
+            Type classType = Type.GetType(className);
+            Type baseType = classType.BaseType;
+            sb.AppendLine($"Base Class: {baseType.Name}");
+
+            MethodInfo[] privateMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (MethodInfo method in privateMethods)
+                sb.AppendLine(method.Name);
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Advanced-C#/C#OOP/Reflection and Attributes - Lab/04. Collector/Spy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersAndSetters(string className)
+        {
+            Type classType = Type.GetType(className);
+            MethodInfo[] classMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("get")))
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("set")))
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
