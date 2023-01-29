25b6b71992bca818293a5174e2dc342c24cb1f99
mmihaylovv87 <111530970+mmihaylovv87@users.noreply.github.com>
2023-01-29 23:46:22 +0200

    Added OOP C# - Reflection&Attributes code.

---
+++ Advanced-C#/C#OOP/Reflection and Attributes - Lab/02. High Quality Mistakes/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] wrongFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (FieldInfo field in wrongFields)
+                sb.AppendLine($"{field.Name} must be private!");
+
+            MethodInfo[] nonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (MethodInfo method in nonPublicMethods.Where(m => m.Name.StartsWith("get")))
+                sb.AppendLine($"{method.Name} have to be public!");
+
+            MethodInfo[] publicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            foreach (MethodInfo method in publicMethods.Where(m => m.Name.StartsWith("set")))
+                sb.AppendLine($"{method.Name} have to be private!");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
